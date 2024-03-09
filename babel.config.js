b95d1e073012aaf7a4df42fd3158ab91a6ab286e
Anderson Preza Martins <lordearena@gmail.com>
2024-03-09 12:08:18 +0100

    Update babel.config.js
    This version provides clearer explanations of each part of the Babel configuration, ensuring that it's more understandable and less prone to errors.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
+// Babel configuration for JavaScript code transpilation
 module.exports = {
-    presets: [
-        ['@babel/preset-env', {
-            targets: {
-                node: 'current'
-            }
-        }]
-    ]
-}
+  // List of presets to be applied during transpilation
+  presets: [
+    // Use the @babel/preset-env preset to transpile code for specific environments
+    ['@babel/preset-env', {
+      // Define the target environments for transpilation
+      targets: {
+        // Transpile the code for the current version of Node.js
+        node: 'current'
+      }
+    }]
+  ]
+};
